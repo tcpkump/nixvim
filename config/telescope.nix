4519e9f49cdc2937dd7655a0e68f4f5b84dbda7d
Garrett Leber <garrett.leber@openvpn.net>
2024-10-08 10:28:29 -0400

    fix: move telescope keymaps into telescope file, add live_grep_args
    live_grep_args allows one to add args to grep, mostly added to enable
    the ability to search in subdirectories

--- config/telescope.nix
+++ config/telescope.nix
@@ -1,6 +1,46 @@
 {
   plugins.telescope = {
     enable = true;
-    extensions.undo.enable = true;
+    extensions = {
+      undo.enable = true;
+      live-grep-args.enable = true;
+    };
   };
+
+  keymaps = [
+    {
+      key = "<leader>ff";
+      action = "<cmd>Telescope find_files<CR>";
+      options.desc = "Find string in filenames";
+      mode = "n";
+    }
+
+    {
+      key = "<leader>fw";
+      action = "<cmd>Telescope live_grep_args<CR>";
+      options.desc = "Grep string in files";
+      mode = "n";
+    }
+
+    {
+      key = "<leader>fg";
+      action = "<cmd>Telescope git_status<CR>";
+      options.desc = "Search modified files";
+      mode = "n";
+    }
+
+    {
+      key = "<leader>fu";
+      action = "<cmd>Telescope undo<CR>";
+      options.desc = "Grep string in undotree";
+      mode = "n";
+    }
+
+    {
+      key = "gr";
+      action = "<cmd>Telescope lsp_references<CR>";
+      options.desc = "Find references";
+      mode = "n";
+    }
+  ];
 }

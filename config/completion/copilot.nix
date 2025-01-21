a41d1baf163ed78fc4d22f8f640c6fb45d2af6d6
Garrett Leber <garrett.leber@openvpn.net>
2025-01-21 14:12:00 -0500

    Config changes for update

--- config/completion/copilot.nix
+++ config/completion/copilot.nix
@@ -4,18 +4,20 @@
   };
   plugins.copilot-lua = {
     enable = true;
-    suggestion = {enabled = false;};
-    panel = {enabled = false;};
-    filetypes = {
-      yaml = true;
-      markdown = true;
-      help = false;
-      gitcommit = false;
-      gitrebase = false;
-      hgcommit = false;
-      svn = false;
-      cvs = false;
-      "." = false;
+    settings = {
+      suggestion.enabled = false;
+      panel.enabled = false;
+      filetypes = {
+        yaml = true;
+        markdown = true;
+        help = false;
+        gitcommit = false;
+        gitrebase = false;
+        hgcommit = false;
+        svn = false;
+        cvs = false;
+        "." = false;
+      };
     };
   };
 }
--- config/default.nix
+++ config/default.nix
@@ -28,6 +28,5 @@
     ./treesitter.nix
     ./trouble.nix
     ./welcome.nix
-    ./wiki.nix
   ];
 }

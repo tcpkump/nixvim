7c06e67dff6bea55ec7c5226b35eef7c19e4a63e
Garrett Leber <garrett@leberra.com>
2025-01-31 13:06:26 -0500

    Add mini.ai, simplify mini.comment config since I use defaults

--- config/mini.nix
+++ config/mini.nix
@@ -2,14 +2,8 @@
   plugins.mini = {
     enable = true;
     modules = {
-      comment = {
-        mappings = {
-          comment = "gc";
-          comment_line = "gcc";
-          comment_visual = "gc";
-          text_object = "gc";
-        };
-      };
+      ai = {};
+      comment = {};
     };
   };
 }

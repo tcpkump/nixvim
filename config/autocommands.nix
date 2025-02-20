853117ac36f3d78379de2d2d2df0c1b9097ce687
Garrett Leber <garrett.leber@openvpn.net>
2025-02-20 13:12:13 -0500

    feat: Enable wrapping on markdown files

--- config/autocommands.nix
+++ config/autocommands.nix
@@ -5,5 +5,10 @@
       event = [ "BufEnter" "BufWinEnter" ];
       pattern = [ "*.tf" "*.tfvars" ];
     }
+    {
+      event = [ "FileType" ];
+      pattern = [ "markdown" ];
+      command = "lua vim.opt_local.wrap = true; vim.opt_local.linebreak = true";
+    }
   ];
 }

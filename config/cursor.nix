3aa089907fc18d91c2e0f0c29bf8ae803f61f8f5
Garrett Leber <garrett@leberra.com>
2025-05-03 16:49:47 -0400

    fix: remove neoscroll, movement too slow for me
    Honestly I have my doubts about anyone easily following what someone is
    doing in neovim during a screenshare anyway :)

--- config/cursor.nix
+++ config/cursor.nix
@@ -1,6 +1,4 @@
 {
   # Highlights current word and line under cursor
   plugins.cursorline.enable = true;
-  # Smoother scrolling for larger movements, improved screensharing experience for reader
-  plugins.neoscroll.enable = true;
 }
